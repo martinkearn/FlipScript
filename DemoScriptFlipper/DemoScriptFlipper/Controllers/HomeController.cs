1292a7627c7d78c48122562325cb0be429110dd8
Martin Kearn <martin.kearn@microsoft.com>
2016-01-12 10:00:43 +0000

    Initial tinkering

---
+++ DemoScriptFlipper/DemoScriptFlipper/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing;
+using System.Drawing.Imaging;
+using CommonMark;
+
+namespace DemoScriptFlipper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //read and chop up MD file
+            string md = System.IO.File.ReadAllText(Server.MapPath("~/SimpleSample.md"));
+
+
+            // parse markdown into document structure
+            var document = CommonMarkConverter.Parse(md);
+
+            // walk the document node tree
+            List<string> tags = new List<string>();
+            foreach (var node in document.AsEnumerable().Where(n => n.Block != null))
+            {
+                if (node.Block != null)
+                {
+                    tags.Add(node.Block.Tag.ToString());
+                }
+            }
+
+
+            //string[] mdSections = md.Split('#');
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DemoScriptFlipper/DemoScriptFlipper/SimpleSample.md
@@ -0,0 +1,23 @@
+﻿#Section 1
+This is a sample section, it has two sub-sections
+
+##SubSection 1.1
+This is sub-section 1.1
+
+##SubSection 1.2
+This is sub-section 1.2
+
+#Section 2
+This is a sample section, it has three sub-sections
+
+##SubSection 2.1
+This is sub-section 2.1
+
+##SubSection 2.2
+This is sub-section 2.2
+
+##SubSection 2.3
+This is sub-section 2.3
+
+#Section 3
+This is a section without any sub sections

fba4bc1dbc39bf9316426669f0d5443627c64328
Martin Kearn <martin.kearn@microsoft.com>
2016-01-14 07:58:01 +0000

    Basic operation wokring. Rendering tabs and html inside them. Spltting on H2 not right though

--- DemoScriptFlipper/DemoScriptFlipper/Controllers/HomeController.cs
+++ DemoScriptFlipper/DemoScriptFlipper/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ using System.Drawing.Imaging;
 using CommonMark;
 using System.Text.RegularExpressions;
 using System.IO;
+using HtmlAgilityPack;
+using System.Text;
+using DemoScriptFlipper.Models;
 
 namespace DemoScriptFlipper.Controllers
 {
@@ -15,26 +18,75 @@ namespace DemoScriptFlipper.Controllers
     {
         public ActionResult Index()
         {
-            //read and chop up MD file
-            string md = System.IO.File.ReadAllText(Server.MapPath("~/SimpleSample.md"));
+            //read file
+            string fileMd = System.IO.File.ReadAllText(Server.MapPath("~/Sample.md"));
 
-            string[] h1Sections = Regex.Split(md, @"(?=^#[^#])", RegexOptions.Multiline);
-            var fullHtml = string.Empty;
-            foreach (string h1Section in h1Sections)
+            //get full html string from md
+            var fileHTML = string.Empty;
+            using (var writer = new StringWriter())
             {
-                var document = CommonMarkConverter.Parse(h1Section);
-                using (var writer = new StringWriter())
-                {
-                    CommonMarkConverter.ProcessStage3(document, writer);
-                    fullHtml += writer.ToString();
-                }
-
-
+                CommonMarkConverter.ProcessStage3(CommonMarkConverter.Parse(fileMd), writer);
+                fileHTML += writer.ToString();
             }
 
-            return Content(fullHtml);
+            //view model
+            var viewModel = new IndexViewModel()
+            {
+                ConvertedHtml = fileHTML  
+            };
 
-            //return View();
+            return View(viewModel);
+        }
+
+        public ActionResult Headers(string html)
+        {
+            //load html document
+            HtmlDocument agilityDoc = new HtmlDocument();
+            agilityDoc.LoadHtml(html);
+
+            //extract headers
+            var headerElements = agilityDoc.DocumentNode.Descendants("h2").Select(nd => nd.InnerText);
+
+            //output HTML
+            var outputSb = new StringBuilder();
+            outputSb.AppendLine("<ul class=\"tabs\" data-tabs id=\"example-tabs\">");
+            var count = 1;
+            foreach (var headerElement in headerElements)
+            {
+                var activeClass = (count == 1) ? "is-active" : string.Empty;
+                outputSb.AppendLine(string.Format("<li class=\"tabs-title {0}\"><a href=\"#{1}\">{2}</a></li>", activeClass, count, headerElement));
+                count += 1;
+            }
+            outputSb.AppendLine("</ul>");
+
+            return Content(outputSb.ToString());
+        }
+
+        public ActionResult Tabs(string html)
+        {
+            //load html document
+            HtmlDocument agilityDoc = new HtmlDocument();
+            agilityDoc.LoadHtml(html);
+
+            //split section by header
+            string[] sections = Regex.Split(html, @"(?=<h2>)", RegexOptions.Multiline);
+
+
+            //output HTML
+            var outputSb = new StringBuilder();
+            outputSb.AppendLine("<div class=\"tabs-content\" data-tabs-content=\"example-tabs\">");
+            var count = 1;
+            foreach (var section in sections)
+            {
+                var activeClass = (count == 1) ? "is-active" : string.Empty;
+                outputSb.AppendLine(string.Format("<div class=\"tabs-panel {0}\" id=\"{1}\">", activeClass, count));
+                outputSb.AppendLine(section);
+                outputSb.AppendLine("</div>");
+                count += 1;
+            }
+            outputSb.AppendLine("</div>");
+
+            return Content(outputSb.ToString());
         }
 
         public ActionResult About()

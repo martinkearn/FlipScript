c3cdfeacc7da3df6b7363b27600df3993efc3b26
Martin Kearn <martin.kearn@microsoft.com>
2016-01-19 07:49:59 +0000

    Removed default about andf contact views

--- FlipScript/src/FlipScript/Controllers/HomeController.cs
+++ FlipScript/src/FlipScript/Controllers/HomeController.cs
@@ -88,20 +88,6 @@ namespace FlipScript.Controllers
             );
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
